a936daa6e13d1fed36143dfb69e42be5a0b145dd
Тимоха <timoha@MacBook-Pro-Timoha.local>
2026-04-18 11:14:24 +0300

    команда баланс

--- main.py
+++ main.py
@@ -13,18 +13,22 @@ user_balance = {}
 
 @bot.event
 async def on_ready():
-    print(f"бот запустился. Привет {bot.user}")
+    print(f"бот запустился. Привет {bot.user} ")
 
 
 @bot.event
 async def on_message(message: Message):
     if message.author == bot.user:   # проверяем написал человек или бот (чтобы бот не отвечал самому себе)
         return
-    await message.channel.send("hello world!")
+    await message.channel.send("Привет!")
     cost = len(message.content) * 10 #получили буквы скоко их
     if user_balance.get(message.author.id,250) <= cost:
         await message.delete()
         return
     user_balance[message.author.id] = user_balance.get(message.author.id,250) - cost
+
+    if message.content == 'balance':     # сказать Николаю то что я попытался
+        await message.channel.send(f"Ваш баланс : {user_balance}")                    # написать чтобы была команда баланс
+
 
 bot.run(os.getenv("TG_API_TOKEN"))
