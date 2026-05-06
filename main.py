0f3ff5ce464160ef2cd3469a7660a91d4932c650
Тимоха <timoha@MacBook-Pro-Timoha.local>
2026-05-06 20:19:48 +0300

    добавление логики корекности перевода для 'perevod'

--- main.py
+++ main.py
@@ -29,6 +29,12 @@ async def on_message(message: Message):
             return
         perevod_comy = int(args[1])
         ckolko_tcoinov = int(args[2])
+        if ckolko_tcoinov < 0:
+            await message.channel.send("братан ну не надо дюпать")
+            return
+        if user_balance[message.author.id] < ckolko_tcoinov:
+            await message.channel.send("у вас нехватает ткойнов")
+            return
         user_balance[message.author.id] = user_balance.get(message.author.id,250) - ckolko_tcoinov
         user_balance[perevod_comy] = user_balance.get(perevod_comy,250) + ckolko_tcoinov
         return
