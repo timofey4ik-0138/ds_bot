58e56b43fdf0ea9084cc557bd3b67293965686dd
Тимоха <timoha@MacBook-Pro-Timoha.local>
2026-05-27 20:57:20 +0300

    исправлена ошибка не нахождение пользователя при удалении роли по реакции ))))))

--- main.py
+++ main.py
@@ -48,27 +48,28 @@ async def on_raw_reaction_remove(payload: disnake.RawReactionActionEvent):
 
     message_id = payload.message_id
     guild = bot.get_guild(payload.guild_id)
+    member = guild.get_member(payload.user_id)
     if message_id == 1497598491793555639:
         if payload.emoji.name == "✅":
             role = guild.get_role(1497604495654387803)
-            await payload.member.remove_roles(role)
+            await member.remove_roles(role)
 
     if message_id == 1501644881733619712:
         if payload.emoji.name == "🟦":
             role = guild.get_role(1497600723917996072)
-            await payload.member.remove_roles(role)
+            await member.remove_roles(role)
 
         if payload.emoji.name == "🟩":
             role = guild.get_role(1497603202936340651)
-            await payload.member.remove_roles(role)
+            await member.remove_roles(role)
 
         if payload.emoji.name == "🟫":
             role = guild.get_role(1497607613322629130)
-            await payload.member.remove_roles(role)
+            await member.remove_roles(role)
 
         if payload.emoji.name == "🟨":
             role = guild.get_role(1497608508902871100)
-            await payload.member.remove_roles(role)
+            await member.remove_roles(role)
 
 
 
